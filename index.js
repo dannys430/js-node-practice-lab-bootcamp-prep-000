c701d4a2963fbdb93986e650e05c676dc2570964
Daniel Singer <dannys430@yahoo.com>
2019-05-27 17:12:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Danny"
-var height = "67"
+var height = "73"
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log(parseInt(height) - 6)
